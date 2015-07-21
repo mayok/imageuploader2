4676aaf29f31ebee4102d56f17a5ad5fc26527d6
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2015-07-21 22:25:30 +0900

    画像をアップロードできるようになった

--- upload.php
+++ upload.php
@@ -1,7 +1,18 @@
 <?php
-#$file = file_get_contents("php://input");
-$file = $_POST["data"];
-$filename = md5_file($file);
+$file = file_get_contents("php://input");
+$tmpfile = '/tmp/imageuploader2';
+file_put_contents($tmpfile, $file);
+$filename = md5_file($tmpfile);
 
-file_put_contents(dirname(__FILE__).'/images/'.$filename, $file);
+// get file extension
+$finfo = finfo_open(FILEINFO_MIME_TYPE);
+$mime_type = finfo_file($finfo, $tmpfile);
+$file_type = explode("/", $mime_type);
+
+if($file_type[0] == "image"){
+  file_put_contents(dirname(__FILE__).'/images/'.$filename.".".$file_type[1], $file);
+  chmod(dirname(__FILE__).'/images/'.$filename.".".$file_type[1], 0644);
+}
+
+unlink($tmpfile);
 ?>
